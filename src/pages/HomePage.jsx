e9fdd7d7854b3bf21d37ca8905060b6f0abfc9ea
Jeremy Dosdo Purba <jeremydosdo@gmail.com>
2025-09-26 14:35:10 +0700

    use axios to fetch the data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,14 @@
+import axios from "axios";
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import checkmark from "../assets/images/icons/checkmark.png";
 import "./HomePage.css";
 
 export function HomePage() {
-  fetch("http://localhost:3000/api/products")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
-    
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  });
+
   return (
     <>
       <link rel="icon" href="images/home-favicon.png"></link>
