0ce0fc493976ddb6e0286b77038d593f2a798f58
Jeremy Dosdo Purba <jeremydosdo@gmail.com>
2025-10-08 14:27:29 +0700

    separate checkout page into smaller components

--- src/pages/checkout/PaymenSummary.jsx
+++ src/pages/checkout/PaymenSummary.jsx
@@ -1,39 +1,8 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { CheckoutHeader } from "./CheckoutHeader";
-import { OrderSummary } from "./OrderSummary";
-import { PaymentSummary } from "./PaymenSummary";
-import "./CheckoutPage.css";
+import { formatMoney } from "../../utils/money"
 
-export function CheckoutPage({ cart }) {
-  const [deliveryOptions, setDeliveryOptions] = useState([]);
-  const [paymentSummary, setPaymentSummary] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get("/api/delivery-options?expand=estimatedDeliveryTime")
-      .then((response) => {
-        setDeliveryOptions(response.data);
-      });
-    axios.get("/api/payment-summary").then((response) => {
-      setPaymentSummary(response.data);
-    });
-  }, []);
+export function PaymentSummary ({paymentSummary}) {
   return (
-    <>
-      <link rel="icon" href="images/cart-favicon.png"></link>
-      <title>Checkout</title>
-
-      <CheckoutHeader />
-
-      <div className="checkout-page">
-        <div className="page-title">Review your order</div>
-
-        <div className="checkout-grid">
-          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
-
-          <PaymentSummary paymentSummary={paymentSummary} />
-          {/* <div className="payment-summary">
+    <div className="payment-summary">
             <div className="payment-summary-title">Payment Summary</div>
 
             {paymentSummary && (
@@ -77,9 +46,6 @@ export function CheckoutPage({ cart }) {
                 </button>
               </>
             )}
-          </div> */}
-        </div>
-      </div>
-    </>
-  );
+          </div>
+  )
 }
