ded46f570de3a1107d1d23515214d5851031367a
Jeremy Dosdo Purba <jeremydosdo@gmail.com>
2025-10-18 14:14:22 +0700

    create update delivery option feature

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -5,7 +5,7 @@ import { OrderSummary } from "./OrderSummary";
 import { PaymentSummary } from "./PaymenSummary";
 import "./CheckoutPage.css";
 
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart, loadCart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
 
@@ -21,7 +21,8 @@ export function CheckoutPage({ cart }) {
     };
 
     fetchCheckoutData();
-  }, []);
+  }, [cart]);
+  
   return (
     <>
       <link rel="icon" href="images/cart-favicon.png"></link>
@@ -33,7 +34,7 @@ export function CheckoutPage({ cart }) {
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
+          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart} />
 
           <PaymentSummary paymentSummary={paymentSummary} />
         </div>
