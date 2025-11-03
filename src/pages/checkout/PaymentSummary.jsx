2e6b2908955941ae3431d5417d25fa83dfba5ca5
Jeremy Dosdo Purba <jeremydosdo@gmail.com>
2025-11-03 13:49:11 +0700

    clean up some test code for PaymentSummary

--- src/pages/checkout/PaymentSummary.jsx
+++ src/pages/checkout/PaymentSummary.jsx
@@ -1,61 +1,80 @@
-import axios from "axios"
+import axios from "axios";
 import { useNavigate } from "react-router";
-import { formatMoney } from "../../utils/money"
+import { formatMoney } from "../../utils/money";
 
-export function PaymentSummary ({paymentSummary, loadCart}) {
-  const navigate = useNavigate()
+export function PaymentSummary({ paymentSummary, loadCart }) {
+  const navigate = useNavigate();
   const createOrder = async () => {
     await axios.post("/api/orders");
-    await loadCart()
-    navigate("/orders")
-  }
+    await loadCart();
+    navigate("/orders");
+  };
 
   return (
     <div className="payment-summary">
-            <div className="payment-summary-title">Payment Summary</div>
+      <div className="payment-summary-title">Payment Summary</div>
 
-            {paymentSummary && (
-              <>
-                <div className="payment-summary-row">
-                  <div data-testid="payment-summary-total-items">Items ({paymentSummary.totalItems}):</div>
-                  <div className="payment-summary-money" data-testid="payment-summary-price">
-                    {formatMoney(paymentSummary.productCostCents)}
-                  </div>
-                </div>
-                <div className="payment-summary-row">
-                  <div>Shipping &amp; handling:</div>
-                  <div className="payment-summary-money" data-testid="payment-summary-shipping">
-                    {formatMoney(paymentSummary.shippingCostCents)}
-                  </div>
-                </div>
-
-                <div className="payment-summary-row subtotal-row">
-                  <div>Total before tax:</div>
-                  <div className="payment-summary-money" data-testid="payment-summary-before-tax">
-                    {formatMoney(paymentSummary.totalCostBeforeTaxCents)}
-                  </div>
-                </div>
-
-                <div className="payment-summary-row">
-                  <div>Estimated tax (10%):</div>
-                  <div className="payment-summary-money" data-testid="payment-summary-tax">
-                    {formatMoney(paymentSummary.taxCents)}
-                  </div>
-                </div>
-
-                <div className="payment-summary-row total-row">
-                  <div>Order total:</div>
-                  <div className="payment-summary-money" data-testid="payment-summary-total">
-                    {formatMoney(paymentSummary.totalCostCents)}
-                  </div>
-                </div>
-
-                <button className="place-order-button button-primary"
-                onClick={createOrder}>
-                  Place your order
-                </button>
-              </>
-            )}
+      {paymentSummary && (
+        <>
+          <div
+            className="payment-summary-row"
+            data-testid="payment-summary-price"
+          >
+            <div data-testid="payment-summary-total-items">
+              Items ({paymentSummary.totalItems}):
+            </div>
+            <div className="payment-summary-money">
+              {formatMoney(paymentSummary.productCostCents)}
+            </div>
           </div>
-  )
+          <div
+            className="payment-summary-row"
+            data-testid="payment-summary-shipping"
+          >
+            <div>Shipping &amp; handling:</div>
+            <div className="payment-summary-money">
+              {formatMoney(paymentSummary.shippingCostCents)}
+            </div>
+          </div>
+
+          <div
+            className="payment-summary-row subtotal-row"
+            data-testid="payment-summary-before-tax"
+          >
+            <div>Total before tax:</div>
+            <div className="payment-summary-money">
+              {formatMoney(paymentSummary.totalCostBeforeTaxCents)}
+            </div>
+          </div>
+
+          <div
+            className="payment-summary-row"
+            data-testid="payment-summary-tax"
+          >
+            <div>Estimated tax (10%):</div>
+            <div className="payment-summary-money">
+              {formatMoney(paymentSummary.taxCents)}
+            </div>
+          </div>
+
+          <div
+            className="payment-summary-row total-row"
+            data-testid="payment-summary-total"
+          >
+            <div>Order total:</div>
+            <div className="payment-summary-money">
+              {formatMoney(paymentSummary.totalCostCents)}
+            </div>
+          </div>
+
+          <button
+            className="place-order-button button-primary"
+            onClick={createOrder}
+          >
+            Place your order
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
--- src/pages/checkout/PaymentSummary.test.jsx
+++ src/pages/checkout/PaymentSummary.test.jsx
@@ -1,14 +1,14 @@
 import { vi, it, expect, describe, beforeEach } from "vitest";
-import { PaymentSummary } from "./PaymentSummary";
+import { render, screen, within } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
-import { render, screen } from "@testing-library/react";
+import { PaymentSummary } from "./PaymentSummary";
 import { formatMoney } from "../../utils/money";
 
 vi.mock("axios");
 
 describe("Payment Summary", () => {
   let paymentSummary;
-  const loadCart = vi.fn();
+  let loadCart;
 
   beforeEach(() => {
     paymentSummary = {
@@ -19,31 +19,92 @@ describe("Payment Summary", () => {
       taxCents: 864,
       totalCostCents: 9499,
     };
+
+    loadCart = vi.fn();
+
     render(
       <MemoryRouter>
         <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
       </MemoryRouter>
     );
   });
-  
-  it("shows the correct data", () => {
-    expect(screen.getByTestId("payment-summary-total-items")).toHaveTextContent(
-      `Items (${paymentSummary.totalItems})`
-    );
-    expect(screen.getByTestId("payment-summary-price")).toHaveTextContent(
+
+  it("shows the total items correctly", () => {
+    const container = screen.getByTestId("payment-summary-total-items");
+    // Using 'toHaveTextContent'
+    expect(container).toHaveTextContent(`Items (${paymentSummary.totalItems})`);
+
+    // Using 'getByText'
+    expect(screen.getByText(`Items (${paymentSummary.totalItems}):`))
+      .toBeInTheDocument;
+  });
+
+  it("shows the price before tax and shipping correctly", () => {
+    const container = screen.getByTestId("payment-summary-price");
+
+    // Using 'toHaveTextContent'
+    expect(container).toHaveTextContent(
       formatMoney(paymentSummary.productCostCents)
     );
-    expect(screen.getByTestId("payment-summary-shipping")).toHaveTextContent(
+
+    // Using 'getByText
+    expect(
+      within(container).getByText(formatMoney(paymentSummary.productCostCents))
+    ).toBeInTheDocument;
+  });
+
+  it("shows the shipping price correctly", () => {
+    const container = screen.getByTestId("payment-summary-shipping");
+
+    // Using 'toHaveTextContent'
+    expect(container).toHaveTextContent(
       formatMoney(paymentSummary.shippingCostCents)
     );
-    expect(screen.getByTestId("payment-summary-before-tax")).toHaveTextContent(
+
+    // Using 'getByText'
+    expect(
+      within(container).getByText(formatMoney(paymentSummary.shippingCostCents))
+    ).toBeInTheDocument;
+  });
+
+  it("shows total cost before tax correctly", () => {
+    const container = screen.getByTestId("payment-summary-before-tax");
+
+    // Using 'toHaveTextContent'
+    expect(container).toHaveTextContent(
       formatMoney(paymentSummary.totalCostBeforeTaxCents)
     );
-    expect(screen.getByTestId("payment-summary-tax")).toHaveTextContent(
-      formatMoney(paymentSummary.taxCents)
-    );
-    expect(screen.getByTestId("payment-summary-total")).toHaveTextContent(
+
+    // Using 'getByText'
+    expect(
+      within(container).getByText(
+        formatMoney(paymentSummary.totalCostBeforeTaxCents)
+      )
+    ).toBeInTheDocument;
+  });
+
+  it("shows the tax correctly", () => {
+    const container = screen.getByTestId("payment-summary-tax");
+
+    // Using 'toHaveTextContent'
+    expect(container).toHaveTextContent(formatMoney(paymentSummary.taxCents));
+
+    // Using 'getByText'
+    expect(within(container).getByText(formatMoney(paymentSummary.taxCents)))
+      .toBeInTheDocument;
+  });
+
+  it("shows the order total price correctly", () => {
+    const container = screen.getByTestId("payment-summary-total");
+    // Using data-testid
+
+    expect(container).toHaveTextContent(
       formatMoney(paymentSummary.totalCostCents)
-    )
+    );
+
+    // Using 'getByText'
+    expect(
+      within(container).getByText(formatMoney(paymentSummary.totalCostCents))
+    ).toBeInTheDocument;
   });
 });
